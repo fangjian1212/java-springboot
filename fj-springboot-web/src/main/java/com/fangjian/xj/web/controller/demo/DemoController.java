c6129ea207e6c9fed2abb5b0909fe5eb822fbcd0
方健 <jian.fang@chinaredstar.com>
2017-12-12 19:50:46 +0800

    session demo

--- fj-springboot-web/src/main/java/com/fangjian/xj/web/controller/demo/DemoController.java
+++ fj-springboot-web/src/main/java/com/fangjian/xj/web/controller/demo/DemoController.java
@@ -1,12 +1,17 @@
 package com.fangjian.xj.web.controller.demo;
 
 import com.fangjian.framework.rest.result.vo.RestResultVO;
+import com.fangjian.share.session.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @description:
@@ -31,6 +36,41 @@ public class DemoController {
     public RestResultVO<Object> demo(@RequestParam Integer id) {
         LOGGER.info("id:[{}]", id);
         return new RestResultVO(id);
+    }
+
+    /**
+     * demo2
+     *
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/demo2", method = {RequestMethod.GET})
+    public RestResultVO<Object> demo2(HttpServletRequest request) {
+
+        LOGGER.info("id:[{}]", request.getUserPrincipal().getName());
+
+        return new RestResultVO(request.getUserPrincipal().getName() + "|" + request.getSession().getId());
+    }
+
+
+    /**
+     * demo3
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/demo3", method = {RequestMethod.POST})
+    public RestResultVO<Object> demo3(HttpServletRequest request, HttpServletResponse response) {
+
+        HttpSession session = request.getSession();
+        LOGGER.info("session:[{}]", session.getId());
+
+        session.setAttribute(Constants.DEFAULT_PRINCIPAL_SESSION_KEY, "userid");//这里的userid登陆时候，放入具体值
+
+        response.setHeader("x-auth-token", session.getId());
+
+        return new RestResultVO(session.getId());
     }
 
 
