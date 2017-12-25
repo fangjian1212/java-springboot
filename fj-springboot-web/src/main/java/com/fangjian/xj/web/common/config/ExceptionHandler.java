9069a72f524d8ffbbb318264bfedb80bc6c2e271
方健 <jian.fang@chinaredstar.com>
2017-12-25 10:04:53 +0800

    @Valid

--- fj-springboot-web/src/main/java/com/fangjian/xj/web/common/config/ExceptionHandler.java
+++ fj-springboot-web/src/main/java/com/fangjian/xj/web/common/config/ExceptionHandler.java
@@ -5,20 +5,24 @@ import com.fangjian.framework.rest.result.vo.RestResultVO;
 import com.fangjian.framework.utils.self.json.JsonUtil;
 import com.fangjian.xj.service.common.enums.XjServiceCodeEnum;
 import com.fangjian.xj.service.common.exception.XjServiceException;
-import com.fangjian.xj.web.common.exception.XjRsvControllerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.UndeclaredThrowableException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @date:15/12/22 上午10:18
@@ -30,7 +34,7 @@ import java.util.HashMap;
 @Configuration
 public class ExceptionHandler implements HandlerExceptionResolver {
 
-    private static Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandler.class);
 
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
                                          Exception ex) {
@@ -49,24 +53,27 @@ public class ExceptionHandler implements HandlerExceptionResolver {
                 restResultVo.setCode(XjServiceCodeEnum.UNKNOWN_ERROR.getCode());
                 restResultVo.setMessage(XjServiceCodeEnum.UNKNOWN_ERROR.getDesc() + "|" + e.getMessage());
             }
-        } else if (ex instanceof XjRsvControllerException) {
-            restResultVo.setCode(RestResultCode.C400.getCode());
-            restResultVo.setMessage(RestResultCode.C400.getDesc() + "|" + ex.getMessage());
-        } else if (ex instanceof UndeclaredThrowableException) {
-            Throwable throwable = ((UndeclaredThrowableException) ex).getUndeclaredThrowable();
-            if (throwable instanceof XjRsvControllerException) {
-                restResultVo.setCode(RestResultCode.C400.getCode());
-                restResultVo.setMessage(RestResultCode.C400.getDesc() + "|" + throwable.getMessage() + "|" + ex.getMessage());
-            } else {
-                restResultVo.setCode(RestResultCode.C500.getCode());
-                restResultVo.setMessage(RestResultCode.C500.getDesc() + "|" + throwable.getMessage() + "|" + ex.getMessage());
-                logger.error("500 error###:{}", ex);
+        } else if (ex instanceof MethodArgumentNotValidException) {
+            LOGGER.info("MethodArgumentNotValidException...");
+            MethodArgumentNotValidException e = (MethodArgumentNotValidException) ex;
+            List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+            Map<String, String> msg = new LinkedHashMap<>();
+            if (!CollectionUtils.isEmpty(fieldErrors)) {
+                for (FieldError fieldError : fieldErrors) {
+                    if (null == fieldError) {
+                        continue;
+                    }
+                    msg.put(fieldError.getField(), fieldError.getDefaultMessage());
+                }
             }
+            restResultVo.setCode(RestResultCode.C400.getCode());
+            restResultVo.setMessage(RestResultCode.C400.getDesc());
+            restResultVo.setDataMap(msg.toString());
         } else {
             restResultVo.setCode(RestResultCode.C500.getCode());
             restResultVo.setMessage(RestResultCode.C500.getDesc() + "|" + ex.getMessage());
             restResultVo.setDataMap(ex);
-            logger.error("500 error===:{}", ex);
+            LOGGER.error("500 error===:{}", ex);
         }
 
 
